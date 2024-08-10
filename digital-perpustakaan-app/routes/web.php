3025a62988a3ba9c2a89f66115751eaf9a34f6d7
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-08-11 01:14:45 +0700

    Feat: Auth User & Admin

--- digital-perpustakaan-app/routes/web.php
+++ digital-perpustakaan-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,14 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'user'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [AdminController::class, 'index'])->middleware(['auth', 'admin']);
